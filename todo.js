fa82549fa5ac633a35f06fce54973b3391d33eff
KingMeTo92 <34463714+dramon4@users.noreply.github.com>
2019-01-08 21:43:32 +0900

    Update todo.js
    삭제 기능 추가 (localStorage Todos[]
    삭제)
    filter: 조건을 전달하는 항목의 배열을 반환한다.

--- todo.js
+++ todo.js
@@ -3,8 +3,26 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
-const toDos = []; //toDos = 할일 목록을 배열 값으로 선언 해준다.
+let toDos = []; //toDos = 할일 목록을 배열 값으로 선언 해준다.
 
+// function filterFn(toDo) {
+//   // filter는 마치 ForEach에서 Function을 실행하는 것 같이 각각의 item과 같이 실행이 될거다.
+//   //filter가 하는 것은 'array'를 하나 만든다.
+//   return toDo.id === 1;
+// }
+
+function deleteToDo(evnet) {
+  //삭제버튼 함수이다.
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo) {
+    // console.log(toDo.id, li.id);
+    return toDo.id !== parseInt(li.id); //모든 toDos가 li의 id와 같지 않을때..
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); //JSON.stringify는 자바스크립트 object를 String으로 바꿔준다.
 }
@@ -15,6 +33,7 @@ function paintToDo(text) {
   const span = document.createElement("span");
   const newId = toDos.length + 1;
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo); //delBtn을 Click하면 deleteToDo 함수를 호출한다.
   span.innerText = text;
   li.appendChild(delBtn); //appendChild ->무언가를 그의 father element 안에 넣는다.
   li.appendChild(span);
